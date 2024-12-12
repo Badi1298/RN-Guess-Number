c91c51d8cc79fb7a32b7b6a2b695c687e51c1d12
Badi1298 <serbandavid83@yahoo.com>
2024-12-12 16:20:15 +0200

    components layout

---
+++ screens/StartGameScreen.tsx
@@ -0,0 +1,12 @@
+import { TextInput, View } from 'react-native';
+
+type Props = {};
+
+export default function StartGameScreen({}: Props) {
+	return (
+		<View>
+			<TextInput />
+			{/* CUSTOM BUTTONS HERE */}
+		</View>
+	);
+}
