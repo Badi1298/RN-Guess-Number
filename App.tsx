1db2ca8ccdecf1c012b17706290f1b8e99994d14
Badi1298 <serbandavid83@yahoo.com>
2024-12-13 20:54:14 +0200

    background image + gradient

--- App.tsx
+++ App.tsx
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet } from 'react-native';
+
+import { LinearGradient } from 'expo-linear-gradient';
+
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
 	return (
-		<StartGameScreen />
-		// <StatusBar style="auto" />
+		<LinearGradient
+			style={styles.rootScreen}
+			colors={['#4e0329', '#ddb52f']}
+		>
+			<ImageBackground
+				resizeMode="cover"
+				style={styles.rootScreen}
+				imageStyle={styles.backgroundImage}
+				source={require('./assets/images/background.png')}
+			>
+				<StartGameScreen />
+			</ImageBackground>
+			<StatusBar style="light" />
+		</LinearGradient>
 	);
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	rootScreen: {
+		flex: 1,
+	},
+
+	backgroundImage: {
+		opacity: 0.2,
+	},
+});
